c07bebc724128c5d5b7a44c37dd3ad12c0c957ef
Victor Rojas <victor.rojas@colorado.edu>
2015-03-03 05:05:05 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,10 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionController::TestCase
-
-  test "should get home" do
-    get :home
-    assert_response :success
+class StaticPagesController < ApplicationController
+  def home
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
+  def help
+  end
+  
+  def about
   end
 end
